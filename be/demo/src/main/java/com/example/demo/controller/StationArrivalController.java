eaae5cb52f6c821c3bdff55ce86fe595dd7eb2f6
8lue <hkahk@naver.com>
2025-07-05 23:56:44 +0900

    250705하경_버스도착유무로직작성중

--- be/demo/src/main/java/com/example/demo/controller/StationArrivalController.java
+++ be/demo/src/main/java/com/example/demo/controller/StationArrivalController.java
@@ -1,7 +1,30 @@
 package com.example.demo.controller;
 
-//@RestContoller
-//@RequestMapping("/station")
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import com.example.demo.service.StationArrivalService;
+
+@RestController
+@RequestMapping("/station")
+@RequiredArgsConstructor
 public class StationArrivalController {
+	private final StationArrivalService stationArrivalService;
+	
+	// POST API 
+	// StationArrivalService에서 사용 중 > 역에 도착했는지 체크 
+	@PostMapping("/check-arrival")
+	public ResponseEntity<String> checkArrival(
+		// @RequestParam == request.getParameter의 기능과 동일 
+		@RequestParam String busId,
+		@RequestParam double lat,
+		@RequestParam double lng
+	){
+		stationArrivalService.checkArrival(busId, lat, lng);
+		return ResponseEntity.ok("역 도착 체크 완료");
+	}
 	
 }
