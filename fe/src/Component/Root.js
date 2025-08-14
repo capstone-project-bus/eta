3c8e5d6e3d3d69c27bee765eac91b186902f5e62
8lue <hkahk@naver.com>
2025-08-14 21:18:55 +0900

    250814_하겅_css수정

--- fe/src/Component/Root.js
+++ fe/src/Component/Root.js
@@ -1,16 +1,53 @@
 import React, { useState } from "react";
 import Station from "./Station";
+import styles from '../css/RootCss.module.css';
+
+const Root = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <div>
+      <div className={styles.blocTabs}>
+  <div
+    className={
+      toggleState === 1
+        ? `${styles.tabs} ${styles.activeTabs}`
+        : styles.tabs
+    }
+    onClick={() => toggleTab(1)}
+  >
+    한양대/마장 노선
+  </div>
+  <div
+    className={
+      toggleState === 2
+        ? `${styles.tabs} ${styles.activeTabs}`
+        : styles.tabs
+    }
+    onClick={() => toggleTab(2)}
+  >
+    왕십리 노선
+  </div>
+</div>
 
 
-const Root = ()=>{
-    return(
-
-        <div style={{border:"1px solid orange"}}>
-            Tab 자리 
-            <Station />
+      <div className={styles.contentTabs}>
+        <div
+          className={
+            toggleState === 1
+              ? `${styles.content} ${styles.activeContent}`
+              : styles.content
+          }
+        >
+          <Station />
         </div>
-
-    );
-        
+      </div>
+    </div>
+  );
 };
+
 export default Root;
